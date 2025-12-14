8332ff9847204e43f9ad5c8c9e5597bf6ccc986e
Daniel Alp <daniel.alp.email@gmail.com>
2025-12-14 16:59:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Setting up
+```
+apt install tmux
+virtualenv venv
+. venv/bin/activate
+python3 -m pip install -r requirements.txt
+tmux new-session -d -n vm -s vm
+```
+If you are using Visual Studio Code, you can do `ctrl+shift+P` and type `Python: Create Environment` to create a `venv`.\
+If you are on MacOS to install tmux do `brew install tmux`.\
+If you are on Windows, change your operating system.
+Copy your executable (which has to be called `vm`) into the project directory. \
+To test your implementation against my snapshots, do:
+```
+python3 test.py --diff
+```
+If you want to run a specific test and check its output, do:
+```
+python3 test.py --run-test path/to/test/case.in
+```
+# About the tester
+The tester works by running your program and using tmux to send keys to the program, 
+then capturing the contents of the terminal screen and comparing it to the snapshots.
+
+Obviously, there may be differences in how the programs display their output. tmux already takes care of 
+removing any color, bold, etc. The main difference is what we draw in the status line (line at the bottom of the screen). 
+
+The way I compare the screen to the snapshot is by making sure the top $n-1$ lines are identical, and then check that the cursor
+info is correct. To check that the cursor info is correct, I try to look for the `row,col-vcol` pattern on the screen, and assume
+it is the rightmost item on the status line. I also treat cases like `row,0-1` and `row,1` identically (this is not entirely true to vim but it keeps things simple). 
+
+Last thing: my program is not vim compliant in all cases, because that is too difficult, but I tried to only feature tests where my program produced output that was the same as vim.
+
+# About writing your own tests
+If you look at the format of the `.in` files you might see something like this
+```
+ifoobar\nhello, world!<esc>
+```
+Some things to note: if you want to type a newline you have to type `\n` explicitly 
+(and I would recommend doing `\t` for tabs). 
+Newlines at the end of a line are ignored, so
+```
+i
+foobar\n
+hello, world!
+<esc>
+```
+is the same as the test case above.
+To test arrow keys, backspace, and escape, you do 
+```
+<left> <right> <up> <down> <bs> <esc>
+```
+You can also add a multiplier for these special keys, e.g. `<bs 4>`.
+I don't support escaping `<` so it's best you don't type `<` as that will be interpreted as the start of a `<special key>`.
---
+++ test.py
@@ -0,0 +1,136 @@
+import argparse
+import libtmux
+import time
+from pathlib import Path
+
+translate = {
+    "up": "Up",
+    "down": "Down",
+    "left": "Left",
+    "right": "Right",
+    "bs": "^?",
+    "esc": "Escape"
+}
+
+def to_snap_path(test_path: Path) -> Path:
+    return Path("snapshots") / test_path.relative_to("tests").with_suffix(".out")
+
+def test(test_path: str, pane: libtmux.Pane) -> str:
+    with open(test_path, "r") as test_file:        
+        str = "".join(c.rstrip() for c in test_file.readlines())
+        tokens = []
+        token = ""
+        i = 0
+        while i < len(str):
+            if str[i] == "<":
+                tokens.append((token, True))
+                token = ""
+                i += 1
+                while str[i] != ">":
+                    token += str[i] 
+                    i += 1
+                i += 1
+                tokens.append((token, False))
+                token = ""
+            else:
+                token += str[i]
+                i += 1
+        if token:
+            tokens.append((token, True))
+        tokens = [(bytes(token, "utf-8").decode("unicode escape"), is_normal) 
+                  for (token, is_normal) in tokens]
+        for (token, is_normal) in tokens:
+            if is_normal:
+                if ";" in token:
+                    for c in token:
+                        if c == ";":
+                            c = "\\;"
+                        pane.send_keys(c, literal=True, enter=False)
+                        time.sleep(0.05)
+                else:
+                    pane.send_keys(token, literal=True, enter=False)
+                    time.sleep(0.05)
+            else:
+                parts = token.split()
+                repeat = int(parts[1]) if len(parts) == 2 else 1
+                token = translate[parts[0]]
+                for _ in range(repeat):
+                    pane.send_keys(token, literal=False, enter=False)
+                    time.sleep(0.025)
+                        
+    return "\n".join(pane.capture_pane())
+
+def run():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--run-test", type=str)
+    group.add_argument("--diff", action="store_true")
+    group.add_argument("--upgrade", action="store_true")
+    group.add_argument("--clean", action="store_true")
+
+    args = parser.parse_args()
+    server = libtmux.Server()
+    if args.clean:
+        # delete tests
+        snap_paths_keep = set([to_snap_path(path) for path in Path("tests").glob("**/*") if path.is_file()])
+        snap_paths_all = set([path for path in Path("snapshots").glob("**/*") if path.is_file()])
+        snap_paths_clean = snap_paths_all - snap_paths_keep
+        for snap_path in snap_paths_clean:  
+            print(f"\033[32munlink: {snap_path}\033[0m")  
+            snap_path.unlink()
+        # delete empty directories
+        dirs_clean = [dir for dir in Path("snapshots").glob("**/*") if (not dir.is_file()) and (not any(dir.iterdir()))]
+        for dir in dirs_clean:
+            print(f"\033[32mrmdir: {dir}\033[0m")  
+            dir.rmdir()
+        return
+    if args.run_test:
+        session = server.new_session(
+            session_name="vm",
+            kill_session=True,
+            attach=False,
+            window_command="./vm"
+        )
+        print(test(args.run_test, session.active_window.active_pane))
+        return
+    for test_path in Path("tests").glob("**/*"):
+        session = server.new_session(
+            session_name="vm",
+            kill_session=True,
+            attach=False,
+            window_command="vm"
+        )
+        if not Path(test_path).is_file():
+            continue
+        snap_path = to_snap_path(test_path)
+        if args.diff:
+            if not Path(snap_path).exists():
+                print(f"\033[31msnapshot does not exist: {snap_path}\033[0m")
+                continue
+            with open(snap_path, "r") as snap_file:
+                new = test(test_path, session.active_window.active_pane)
+                old = "\n".join(line.rstrip() for line in snap_file.readlines())
+                if new[0:-1] != old[0:-1]:
+                    #fail
+                    print(f"\033[31m{test_path}\033[0m")
+                else:
+                    # compare last line
+                    status_new = new[-1]
+                    status_old = old[-1]
+                    cursor_info_new = status_new.split()[-1]
+                    cursor_info_old = status_old.split()[-1]
+                    row_new, col_new = cursor_info_new.split(",")
+                    row_old, col_old = cursor_info_old.split(",")
+                    if row_new == row_old and (col_new == col_old or (col_new == "0-1" and col_old == "1-1")):
+                        #pass
+                        print(f"\033[32m{test_path}\033[0m")
+                    else:
+                        #fail
+                        print(f"\033[31m{test_path}\033[0m")
+        elif args.upgrade:       
+            snap_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(snap_path, "w") as snap_file:
+                print(f"\033[32mupgrade: {snap_path}\033[0m")
+                snap_file.write(test(test_path, session.active_window.active_pane))
+
+run()
